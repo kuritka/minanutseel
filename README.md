5f9c0fc20069da50d0ac6cc7e1be4ac19c8a142f
kuritka <kuritka@gmail.com>
2017-07-02 11:38:09 +0200

    add images and responsive stuffs

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-"# DaxPilot" 
-
+"MADE IN NUSLE" 
 
 run npm start ; npm run build to prepare dist bundle 
+
+
+poutine
+ https://www.originate.com/
+ https://about.canva.com/
+ http://www.poutine.cz/
+ https://www.primevideo.com/ref=dvm_PDD_WEB_ROW_G_gmat
+ http://zoomtopia.us/
+http://jacobercreative.com/
+http://www.gifn.it/#/ - pekny branding + fonty
+https://www.craftedny.com/responsive-ux-ui-design-nyc-revolt/
+
+
+
+http://placehold.it
+https://codepen.io/Hornebom/pen/GEzDJ
--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 /*eslint-disable import/default */
 
 //to enable application uncomment these!! 
-import 'babel-polyfill';
+// import 'babel-polyfill';
 // import React from 'react';
 // import { render } from 'react-dom';
 // import configureStore from './store/configureStore';
 // import {Provider} from 'react-redux';
 // import { Router, browserHistory } from 'react-router';
 // import routes from './routes';
-//import store from './store';
+// import store from './store';
 // import '../node_modules/toastr/build/toastr.min.css';
 
 
 //import './styles/styles.css'; //Webpack can import CSS files too!
 //import './styles/slider.css';
+
 import './styles/common.scss';
 import './styles/animatedText.scss';
 import './styles/navicon.scss';
 import './styles/font-awesome.min.css'; 
+import './styles/arrowBounce.scss';
+import './styles/sections/main.scss';
+import './styles/sections/team.scss';
+import './styles/sections/overview.scss';
+
+
+
+//import './styles/bounceIn.scss';
+
+
 
 //import './img/Nusle00.jpg'; 
  
